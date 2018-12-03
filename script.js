38759c7650de1cab8405f7ca4e4eebb3bbafd7bf
Eli Gold <e.goldsztejn@eleves-alsacienne.org>
2018-12-03 17:53:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Test heure en live :
 
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-112025576-2');
+
+
+
+
+	let name = prompt("Veuillez entrer votre nom / pseudo", "Invité");
+	if (!name) {
+		name = "Anonyme 🕵️"
+	}
+	let ligne1Statut = "Partagez ce lien à fond ! \n"
+	let ligne2Statut = "bit.ly/2049-jeu"
+	let statut = ligne1Statut + ligne2Statut
+	alert("⚠Le site est en développement. \n Merci de votre patience et de votre fidélité !⚠")
+	alert(statut)
+	swal("Bravo !", "Tu as entré le bon mot de passe.", "success")
+	
