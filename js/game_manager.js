2346dfd05f0c8010746960daab9e1ad58c1ae177
Eli Gold <e.goldsztejn@eleves-alsacienne.org>
2018-12-04 16:55:58 +0100

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,3 +1,5 @@
+// Jeu classique :
+
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
