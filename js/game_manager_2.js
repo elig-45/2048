7edcb79983d2067626929e462794944730ac88c9
Eli Gold <e.goldsztejn@eleves-alsacienne.org>
2018-12-04 16:55:43 +0100

    Update game_manager_2.js

--- js/game_manager_2.js
+++ js/game_manager_2.js
@@ -1,3 +1,6 @@
+// Jeu facile :
+
+
 function GameManager(size, InputManager, Actuator, StorageManager) {
   this.size           = size; // Size of the grid
   this.inputManager   = new InputManager;
