fd8f6bb7104d9498982f3c55799668dd1adc854d
Eli Gold <e.goldsztejn@eleves-alsacienne.org>
2018-12-04 15:14:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ window.dataLayer = window.dataLayer || [];
 
 
 
-	let name = prompt("Veuillez entrer votre nom / pseudo", "Invité");
-	if (!name) {
-		name = "Anonyme 🕵️"
-	}
-	let ligne1Statut = "Partagez ce lien à fond ! \n"
-	let ligne2Statut = "bit.ly/2049-jeu"
-	let statut = ligne1Statut + ligne2Statut
-	alert("⚠Le site est en développement. \n Merci de votre patience et de votre fidélité !⚠")
-	alert(statut)
-	swal("Bravo !", "Tu as entré le bon mot de passe.", "success")
-	swal("#GhobadiPaysan", "Ç"
+let name = prompt("Veuillez entrer votre nom / pseudo", "Invité");
+if (!name) {
+name = "Anonyme 🕵️"
+}
+let ligne1Statut = "Partagez ce lien à fond ! \n"
+let ligne2Statut = "bit.ly/2049-jeu"
+let statut = ligne1Statut + ligne2Statut
+alert("⚠Le site est en développement. \n Merci de votre patience et de votre fidélité !⚠")
+alert(statut)
+swal("Bravo !", "Tu as entré le bon mot de passe.", "success")
